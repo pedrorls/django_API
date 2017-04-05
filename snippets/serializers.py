1c7ad2c6ccf5e8c8bc3cf6764478113782825658
Pedro Rodrigues Lima <perliima72@gmail.com>
2017-04-05 14:50:48 -0300

    serializers class created

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -6,4 +6,25 @@ class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
-    
+    lineos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+    def create(self, validated_data):
+        """
+        Create and return a new snippet instance, given the validated data.
+        """
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return and existing snippet instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.code  = validated_data.get('code', instance.code)
+        instance.lineos = validated_data.get('lineos', instance.lineos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+
+        instance.save()
+        return instance
