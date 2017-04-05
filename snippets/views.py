c19e05ae3eca3f6a1ce2d2e6df303bfbe9b15ceb
Pedro Rodrigues Lima <perliima72@gmail.com>
2017-04-05 18:35:17 -0300

    view to get and post snippet created

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import Snippet
+from .serializers import SnippetSerializer
 
-# Create your views here.
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializers = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializers)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.data, status=400)
+        
