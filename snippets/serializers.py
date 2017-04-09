51fe2b79aaf9dfee2a08043796b43dfcc8463d26
Pedro Rodrigues Lima <perliima72@gmail.com>
2017-04-08 22:05:19 -0300

    serializers modified to associate with snippet

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -4,10 +4,12 @@ from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'lineos', 'language', 'style')
+        fields = ('id', 'title', 'code', 'lineos',
+                  'language', 'style', 'owner')
 
 
 class UserSerializer(serializers.ModelSerializer):
