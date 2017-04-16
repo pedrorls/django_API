d38ab75650d83fa138444417a4f37bd3c14b460e
Pedro Rodrigues Lima <perliima72@gmail.com>
2017-04-16 16:59:03 -0300

    urls now user DefaultRouter from rest_framework

--- snippets/views.py
+++ snippets/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, renderers
+from rest_framework import viewsets, renderers, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import detail_route, api_view
 from django.contrib.auth.models import User
